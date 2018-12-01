bb2e3dfb4ca4696f27a44821715e58a2bc6387fa
meck <johan@meck.se>
2018-12-01 08:54:24 +0100

    Day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,7 +1,24 @@
 module Day1 (day01a, day01b) where
 
+import           Util
+import           Data.Maybe                     ( fromJust )
+import           Data.List                      ( scanl' )
+import           Data.Set                       ( empty
+                                                , insert
+                                                , member
+                                                , Set
+                                                )
+
 day01a :: String -> String
-day01a = id
+day01a = show . sum . parse
 
 day01b :: String -> String
-day01b = id
+day01b = show . fromJust . firstDup empty . scanl' (+) 0 . cycle . parse
+
+firstDup :: Ord a => Set a -> [a] -> Maybe a
+firstDup _ [] = Nothing
+firstDup seen (x : xs) =
+    if x `member` seen then Just x else firstDup (x `insert` seen) xs
+
+parse :: String -> [Int]
+parse = fmap (read . filter (/= '+')) . linesStrip
--- src/Util.hs
+++ src/Util.hs
@@ -1,8 +1,10 @@
-module Util (
-    strip
-) where
+module Util (strip, linesStrip, Data.List.Split.splitOn) where
 
-import qualified Data.Text as T
+import           Data.List.Split                ( splitOn )
+import qualified Data.Text                     as T
 
 strip :: String -> String
 strip = T.unpack . T.strip . T.pack
+
+linesStrip :: String -> [String]
+linesStrip = fmap (T.unpack . T.strip) . T.lines . T.strip . T.pack
