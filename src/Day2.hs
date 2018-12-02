43a9c4ba18af5aba7be5e46512ef0d6b0fc507d1
meck <johan@meck.se>
2018-12-02 09:57:13 +0100

    cleanup Day2

--- src/Day2.hs
+++ src/Day2.hs
@@ -3,8 +3,9 @@ module Day2 (day02a, day02b) where
 import           Util
 import           Data.List                      ( group
                                                 , sort
+                                                , intersect
+                                                , (\\)
                                                 )
-import           Data.Maybe                     ( catMaybes )
 
 day02a :: String -> String
 day02a input = show $ numNDiff 2 * numNDiff 3
@@ -14,14 +15,5 @@ day02a input = show $ numNDiff 2 * numNDiff 3
 
 
 day02b :: String -> String
-day02b input = filtEq $ headNotEq $ linesStrip input
-    where filtEq (a, b) = fmap fst $ filter (uncurry (==)) $ zip a b
-
-headNotEq :: Eq a => [[a]] -> ([a], [a])
-headNotEq ys = head $ catMaybes $ go ys
-  where
-    go (x : xs) = (nNotEq 1 x <$> xs) ++ go xs
-    go []       = []
-    nNotEq n a b | nDiff == n = Just (a, b)
-                 | otherwise  = Nothing
-        where nDiff = length $ filter (uncurry (/=)) $ zip a b
+day02b input = head $ go $ linesStrip input
+    where go xs =  [x `intersect` x' | x <- xs, x' <- xs, length (x \\ x') == 1]
