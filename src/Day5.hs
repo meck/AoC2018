40531036e7d46e1c9b9a5f0b53ab8c513cb67242
meck <johan@meck.se>
2018-12-05 21:01:37 +0100

    Meck Dag 5

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,8 +1,20 @@
 module Day5 (day05a, day05b) where
 
+import           Data.Char                      ( toLower )
+import           Util
+
+filterUnits :: String -> Int
+filterUnits = length . foldr step []
+  where
+    step x (y : xs) | x /= y && toLower x == toLower y = xs
+    step x xs = x : xs
+
+minPoly :: String -> Int
+minPoly str = minimum $ fmap filterUnits $ redPoly <$> ['a' .. 'z']
+    where redPoly c = filter ((c /=) . toLower) str
 
 day05a :: String -> String
-day05a = id
+day05a = show . filterUnits . strip
 
 day05b :: String -> String
-day05b = id
+day05b = show . minPoly . strip
